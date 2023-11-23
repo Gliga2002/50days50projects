7f6e479d34dac90e0b79133c8b237117fdf01da8
Darko Gligorijevic <darkogligorijevic@4gmail.com>
2023-11-23 21:49:49 +0100

    Finish Password Generator project

---
+++ password-generator/script.js
@@ -0,0 +1,91 @@
+const resultEl = document.getElementById('result');
+const lengthEl = document.getElementById('length');
+const uppercaseEl = document.getElementById('uppercase');
+const lowercaseEl = document.getElementById('lowercase');
+const numbersEl = document.getElementById('numbers');
+const symbolsEl = document.getElementById('symbols');
+const generateEl = document.getElementById('generate');
+const clipboardEl = document.getElementById('clipboard');
+
+
+const randomFunc = {
+  lower: getRandomLower,
+  upper: getRandomUpper,
+  number: getRandomNumber,
+  symbol: getRandomSymbol
+}
+
+// copy to clipboard
+clipboardEl.addEventListener('click', () => {
+  const textarea = document.createElement('textarea');
+  const password = resultEl.innerText;
+  
+  if (!password) { return };
+
+  textarea.value = password;
+  document.body.appendChild(textarea);
+  textarea.select();
+  document.execCommand('copy');
+  textarea.remove();
+  alert('The password is coppied to clipoad');
+})
+
+generateEl.addEventListener('click', () => {
+  const length = +lengthEl.value;
+  const hasLower = lowercaseEl.checked;
+  const hasUpper = uppercaseEl.checked;
+  const hasNumber = numbersEl.checked;
+  const hasSymbol = symbolsEl.checked;
+
+  console.log({length, hasLower, hasUpper, hasNumber, hasSymbol})
+  resultEl.innerHTML = generatePassword(hasLower, hasUpper, hasNumber, hasSymbol, length);
+
+})
+
+// ovo je malo tesko
+function generatePassword(lower, upper, number, symbol, length) {
+  let generatedPassword = '';
+  // ovo typesCount mi vraca broj, ovo je + operator sabiranja i true === 1 a false === 0
+  const typesCount = lower + upper + number + symbol;
+
+  // hocu [{lower:true}, {upper: true}, {number: false}, {symbol: true}]
+  // sa filter vrati samo true
+  // Object.values(item) ==> [true] i samo [0] da uznem true
+  const typesArr = [{lower}, {upper}, {number}, {symbol}].filter(item => Object.values(item)[0]);
+
+  console.log(typesArr);
+  
+  if(typesCount === 0) {
+      return ''
+  }
+
+  for(let i = 0; i < length; i += typesCount) {
+      typesArr.forEach(type => {
+          // ovo Object.keys je suportno od Object.values, vraca niz keya [lower] i uzmem [0]
+          const funcName = Object.keys(type)[0]
+          generatedPassword += randomFunc[funcName]()
+      })
+  }
+
+  const finalPassword = generatedPassword.slice(0, length)
+
+  return finalPassword
+}
+
+function getRandomLower() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+function getRandomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+function getRandomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+function getRandomSymbol() {
+  const symbols = '!@#$%^&*(){}[]=<>/,.'
+  return symbols[Math.floor(Math.random() * symbols.length)]
+}
+
